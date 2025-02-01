99edee31090be3fb02e9dff3bad95560151ba65d
vxxxxc <>
2025-02-02 03:29:02 +0800

    comment out unuse keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,18 +2,18 @@ local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
 -- Do things without affecting the registers
-keymap.set("n", "x", '"_x')
-keymap.set("n", "<Leader>p", '"0p')
-keymap.set("n", "<Leader>P", '"0P')
-keymap.set("v", "<Leader>p", '"0p')
-keymap.set("n", "<Leader>c", '"_c')
-keymap.set("n", "<Leader>C", '"_C')
-keymap.set("v", "<Leader>c", '"_c')
-keymap.set("v", "<Leader>C", '"_C')
-keymap.set("n", "<Leader>d", '"_d')
-keymap.set("n", "<Leader>D", '"_D')
-keymap.set("v", "<Leader>d", '"_d')
-keymap.set("v", "<Leader>D", '"_D')
+-- keymap.set("n", "x", '"_x')
+-- keymap.set("n", "<Leader>p", '"0p')
+-- keymap.set("n", "<Leader>P", '"0P')
+-- keymap.set("v", "<Leader>p", '"0p')
+-- keymap.set("n", "<Leader>c", '"_c')
+-- keymap.set("n", "<Leader>C", '"_C')
+-- keymap.set("v", "<Leader>c", '"_c')
+-- keymap.set("v", "<Leader>C", '"_C')
+-- keymap.set("n", "<Leader>d", '"_d')
+-- keymap.set("n", "<Leader>D", '"_D')
+-- keymap.set("v", "<Leader>d", '"_d')
+-- keymap.set("v", "<Leader>D", '"_D')
 
 -- Increment/decrement
 keymap.set("n", "+", "<C-a>")
@@ -29,8 +29,8 @@ keymap.set("n", "<C-a>", "gg<S-v>G")
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
 -- Disable continuations
-keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
-keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+-- keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+-- keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
 
 -- Jumplist
 keymap.set("n", "<C-m>", "<C-i>", opts)
