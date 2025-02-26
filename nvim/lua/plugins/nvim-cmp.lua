018f842992855386a87aabb8c6fd17a8e78f6cbf
vxxxxc <>
2025-02-27 00:03:53 +0800

    change scrolling keymaping

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -39,8 +39,8 @@ return {
         },
         preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
         mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-u>"] = cmp.mapping.scroll_docs(-2),
+          ["<C-d>"] = cmp.mapping.scroll_docs(2),
           ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<C-Space>"] = cmp.mapping.complete(),
@@ -63,6 +63,10 @@ return {
           { name = "buffer" },
         }),
         formatting = {
+          window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered(),
+          },
           format = function(entry, item)
             local icons = LazyVim.config.icons.kinds
             if icons[item.kind] then
