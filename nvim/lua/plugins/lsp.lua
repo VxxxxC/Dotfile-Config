3fc849b557829c2e8e1017dd2c5e92bc21129aae
vxxxxc <vxxxxc@Serpents-M3.local>
2025-04-08 03:50:56 +0800

    update lsp, cmp config for swift lang

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -103,6 +103,8 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      { "antosha417/nvim-lsp-file-operations", config = true },
       {
         "kosayoda/nvim-lightbulb",
         config = function()
@@ -122,6 +124,27 @@ return {
           })
         end,
       },
+      config = function()
+        local lspconfig = require("lspconfig")
+        local util = require("lspconfig.util")
+        local cmp_nvim_lsp = require("cmp_nvim_lsp")
+        local capabilities = cmp_nvim_lsp.default_capabilities()
+        local opts = { noremap = true, silent = true }
+        local on_attach = function(_, bufnr)
+          opts.buffer = bufnr
+
+          opts.desc = "Show line diagnostics"
+          vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+
+          opts.desc = "Show documentation for what is under cursor"
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        end
+
+        lspconfig["sourcekit"].setup({
+          capabilities = capabilities,
+          on_attach = on_attach,
+        })
+      end,
       {
         "Wansmer/symbol-usage.nvim",
         event = "LspAttach",
