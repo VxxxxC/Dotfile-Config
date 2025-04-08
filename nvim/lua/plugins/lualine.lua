67c2612d543470d31d70fd15ed4f94c64a51fb0b
vxxxxc <vxxxxc@Serpents-M3.local>
2025-04-08 19:36:25 +0800

    add swift lualine status and icon

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -447,6 +447,27 @@ local function harper_ls()
   }
 end
 
+local function xcodebuild_device()
+  if vim.g.xcodebuild_platform == "macOS" then
+    return " macOS"
+  end
+
+  local deviceIcon = ""
+  if vim.g.xcodebuild_platform:match("watch") then
+    deviceIcon = "􀟤"
+  elseif vim.g.xcodebuild_platform:match("tv") then
+    deviceIcon = "􀡴 "
+  elseif vim.g.xcodebuild_platform:match("vision") then
+    deviceIcon = "􁎖 "
+  end
+
+  if vim.g.xcodebuild_os then
+    return deviceIcon .. " " .. vim.g.xcodebuild_device_name .. " (" .. vim.g.xcodebuild_os .. ")"
+  end
+
+  return deviceIcon .. " " .. vim.g.xcodebuild_device_name
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   opts = {
@@ -474,6 +495,9 @@ return {
       lualine_x = {
         circle_icon("left"),
         diagnostics(),
+        { "' ' .. vim.g.xcodebuild_last_status", color = { fg = "Gray" } },
+        { "'󰙨 ' .. vim.g.xcodebuild_test_plan", color = { fg = "#a6e3a1", bg = "#161622" } },
+        { xcodebuild_device, color = { fg = "#f9e2af", bg = "#161622" } },
       },
       lualine_y = {
         space(),
