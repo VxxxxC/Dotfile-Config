b3fa8e7e2968b0da9256b270a00339f839cdd22b
vxxxxc <vxxxxc@Serpents-M3.local>
2025-05-16 03:45:44 +0800

    add formatter

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,52 +1,57 @@
 -- Formatting.
 return {
-	{
-		"stevearc/conform.nvim",
-		event = "BufWritePre",
-		opts = {
-			notify_on_error = false,
-			formatters_by_ft = {
-				c = { name = "clangd", timeout_ms = 500, lsp_format = "prefer" },
-				javascript = { "prettier", name = "dprint", timeout_ms = 500, lsp_format = "fallback" },
-				javascriptreact = { "prettier", name = "dprint", timeout_ms = 500, lsp_format = "fallback" },
-				json = { "prettier", name = "dprint", timeout_ms = 500, lsp_format = "fallback" },
-				jsonc = { "prettier", name = "dprint", timeout_ms = 500, lsp_format = "fallback" },
-				less = { "prettier" },
-				lua = { "stylua" },
-				markdown = { "prettier" },
-				rust = { name = "rust_analyzer", timeout_ms = 500, lsp_format = "prefer" },
-				scss = { "prettier" },
-				sh = { "shfmt" },
-				typescript = { "prettier", name = "dprint", timeout_ms = 500, lsp_format = "fallback" },
-				typescriptreact = { "prettier", name = "dprint", timeout_ms = 500, lsp_format = "fallback" },
-				-- For filetypes without a formatter:
-				["_"] = { "trim_whitespace", "trim_newlines" },
-			},
-			format_on_save = function()
-				-- Don't format when minifiles is open, since that triggers the "confirm without
-				-- synchronization" message.
-				if vim.g.minifiles_active then
-					return nil
-				end
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    opts = {
+      notify_on_error = false,
+      formatters_by_ft = {
+        javascript = { "prettierd", name = "dprint", timeout_ms = 500, lsp_format = "fallback" },
+        javascriptreact = { "prettierd", name = "dprint", timeout_ms = 500, lsp_format = "fallback" },
+        json = { "prettierd", name = "dprint", timeout_ms = 500, lsp_format = "fallback" },
+        jsonc = { "prettierd", name = "dprint", timeout_ms = 500, lsp_format = "fallback" },
+        less = { "prettierd" },
+        lua = { "stylua" },
+        markdown = { "prettierd" },
+        rust = { name = "rust_analyzer", timeout_ms = 500, lsp_format = "prefer" },
+        scss = { "prettierd" },
+        sh = { "shfmt" },
+        bash = { "shfmt" },
+        typescript = { "prettierd", name = "dprint", timeout_ms = 500, lsp_format = "fallback" },
+        typescriptreact = { "prettierd", name = "dprint", timeout_ms = 500, lsp_format = "fallback" },
+        yaml = { "yamlfmt" },
+        toml = { "taplo" },
 
-				-- Stop if we disabled auto-formatting.
-				if not vim.g.autoformat then
-					return nil
-				end
+        -- For filetypes without a formatter:
+        ["_"] = { "trim_whitespace", "trim_newlines" },
+      },
+      format_on_save = function()
+        -- Don't format when minifiles is open, since that triggers the "confirm without
+        -- synchronization" message.
+        if vim.g.minifiles_active then
+          return nil
+        end
 
-				return {}
-			end,
-			formatters = {
-				-- Require a Prettier configuration file to format.
-				prettier = { require_cwd = true },
-			},
-		},
-		init = function()
-			-- Use conform for gq.
-			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+        -- Stop if we disabled auto-formatting.
+        if not vim.g.autoformat then
+          return nil
+        end
 
-			-- Start auto-formatting by default (and disable with my ToggleFormat command).
-			vim.g.autoformat = true
-		end,
-	},
+        return {}
+      end,
+      formatters = {
+        -- Require a prettier configuration file to format.
+        prettier = { require_cwd = true },
+        taplo = { command = "taplo", args = { "format" } },
+      },
+    },
+
+    init = function()
+      -- Use conform for gq.
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+      -- Start auto-formatting by default (and disable with my ToggleFormat command).
+      vim.g.autoformat = true
+    end,
+  },
 }
