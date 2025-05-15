b6d2655a4cbeaad298225161446b871c733bbf53
vxxxxc <vxxxxc@Serpents-M3.local>
2025-05-16 05:02:33 +0800

    update json

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,20 @@
 return {
-	{
-		"craftzdog/solarized-osaka.nvim",
-		lazy = false,
-		priority = 1000,
-		opt = {},
-		init = function()
-			-- Set my colorscheme.
-			vim.g.colors_name = "solarized-osaka"
-		end,
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "solarized-osaka",
-		},
-	},
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opt = {
+      transparent = true,
+    },
+    init = function()
+      -- Set my colorscheme.
+      vim.g.colors_name = "solarized-osaka"
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
+    },
+  },
 }
