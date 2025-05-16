2a0038843daf4fa2185ebba93358bc2291cbd8b2
vxxxxc <vxxxxc@Serpents-M3.local>
2025-05-17 03:25:06 +0800

    config blink-cmp for auto-completions & snippets

---
+++ nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,84 @@
+return {
+  {
+    "saghen/blink.cmp",
+    enabled = true,
+    dependencies = { "rafamadriz/friendly-snippets" },
+
+    version = "1.*",
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      ---@module 'blink.cmp'
+      ---@type blink.cmp.config.keymap
+      keymap = {
+        preset = "default",
+        ["<Enter>"] = { "select_and_accept" },
+        ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+        ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+      },
+
+      appearance = {
+        nerd_font_variant = "Nerd Font",
+      },
+
+      completion = {
+        keyword = {
+          range = "prefix",
+        },
+        ghost_text = { enabled = true },
+        documentation = { window = { border = "single" }, auto_show = true },
+        trigger = {
+          show_in_snippet = true,
+          show_on_keyword = true,
+          show_on_trigger_character = true,
+          show_on_blocked_trigger_characters = { " ", "\n", "\t" },
+        },
+        list = {
+          selection = {
+            preselect = true,
+            auto_insert = true,
+          },
+        },
+        menu = {
+          border = "single",
+          draw = {
+            -- We don't need label_description now because label and label_description are already
+            -- combined together in label by colorful-menu.nvim.
+            columns = { { "kind" }, { "kind_icon" }, { "label", gap = 1 } },
+            components = {
+              label = {
+                text = function(ctx)
+                  return require("colorful-menu").blink_components_text(ctx)
+                end,
+                highlight = function(ctx)
+                  return require("colorful-menu").blink_components_highlight(ctx)
+                end,
+              },
+            },
+            treesitter = { "lsp" },
+          },
+        },
+      },
+      signature = { window = { border = "single" } },
+
+      sources = {
+        providers = {
+          lsp = {
+            name = "lsp",
+            enabled = true,
+            module = "blink.cmp.sources.lsp",
+          },
+          snippets = {
+            name = "snippets",
+            enabled = true,
+            module = "blink.cmp.sources.snippets",
+          },
+        },
+      },
+
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+    },
+    opts_extend = { "sources.default" },
+  },
+}
