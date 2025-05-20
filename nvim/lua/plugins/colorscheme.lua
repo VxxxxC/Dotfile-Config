e5476345ceb543f6ff1eb8fb8b80e22858d678df
vxxxxc <vxxxxc@Serpents-M3.local>
2025-05-21 03:15:17 +0800

    change back to origin colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,31 +1,12 @@
 return {
-  --[[ {
+  {
     "craftzdog/solarized-osaka.nvim",
     lazy = false,
     priority = 1000,
-    opt = {
-      transparent = true,
-    },
     init = function()
       -- Set my colorscheme.
       vim.g.colors_name = "solarized-osaka"
       vim.cmd("colorscheme solarized-osaka")
-    end,
-  }, ]]
-  {
-    "oxfist/night-owl.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("night-owl").setup({
-        bold = true,
-        italics = true,
-        underline = true,
-        undercurl = true,
-        transparent_background = true,
-      })
-      vim.g.colors_name = "night-owl"
-      vim.cmd("colorscheme night-owl")
     end,
   },
 }
