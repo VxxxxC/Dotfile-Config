dbfc1e7fe8dd3d4bcf0537e8bb07b33316d5e104
vxxxxc <vxxxxc@Serpents-M3.local>
2025-06-18 04:10:10 +0800

    update

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -9,6 +9,8 @@ return {
       { "<leader>tt", require("fzf-lua").tmux_buffers, desc = "List Tmux Paste Buffer", mode = "n" },
       { "<leader>q", group = "Exit", icon = require("config.theme").icons.exit2 },
       { "<leader>qq", "<cmd>quitall<CR>", desc = "Quit All" },
+      { "<leader>f", group = "Fzf History/Buffer/Help", icon = require("config.theme").icons.find },
+      { "<leader>F", group = "Flash", icon = require("config.theme").icons.flash },
     })
   end,
 }
